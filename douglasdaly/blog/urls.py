1cdbb61785604e1dd7b8a0c81ca730b514326f4d
Doug <douglas.daly89@gmail.com>
2017-12-04 19:01:04 -0500

    Remove duplicate 'blog' path

--- douglasdaly/blog/urls.py
+++ douglasdaly/blog/urls.py
@@ -14,12 +14,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(
-            r'^blog/view/(?P<slug>[^\.]+).html',
+            r'^view/(?P<slug>[^\.]+).html',
             views.view_post,
             name='view_blog_post'
     ),
     url(
-            r'^blog/category/(?P<slug>[^\.]+).html',
+            r'^category/(?P<slug>[^\.]+).html',
             views.view_category,
             name='view_blog_category'
     ),
