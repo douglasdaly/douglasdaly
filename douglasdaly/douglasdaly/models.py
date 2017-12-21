35f4b76a0991a06acdcb6277649564a9a8b77b82
Doug <douglas.daly89@gmail.com>
2017-12-20 22:37:13 -0500

    Fixes to get content below navbar

--- douglasdaly/douglasdaly/models.py
+++ douglasdaly/douglasdaly/models.py
@@ -25,14 +25,17 @@ class Page(SortableMixin):
     slug = models.SlugField(max_length=80, unique=True)
     link_name = models.CharField(max_length=40, unique=True)
     passthrough_page = models.BooleanField(default=False)
-    passthrough_link = models.CharField(max_length=40, default=None, null=True)
-    custom_css = models.CharField(max_length=80, default=None, null=True)
+    passthrough_link = models.CharField(max_length=40, default=None, null=True,
+                                        blank=True)
+    custom_css = models.CharField(max_length=80, default=None, null=True,
+                                  blank=True)
     content = models.TextField(default=None, null=True)
 
     class Meta:
         ordering = ['the_order']
 
-    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False,
+                                            db_index=True)
 
     def __str__(self):
         return self.title
