9266ca560d594392eebcd3de8ec77c32caf914b9
Doug <douglas.daly89@gmail.com>
2018-01-06 14:15:47 -0500

    Code style updates

--- douglasdaly/blog/templatetags/markdownify.py
+++ douglasdaly/blog/templatetags/markdownify.py
@@ -23,10 +23,11 @@ class HighlightRenderer(mistune.Renderer):
 
     def block_code(self, code, lang=None):
         if not lang:
-            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+            return '\n<div class="highlight"><pre><code>' + \
+                   '%s</code></pre></div>\n' % mistune.escape(code)
         else:
             lexer = get_lexer_by_name(lang, stripall=True)
-            formatter = HtmlFormatter()
+            formatter = HtmlFormatter(linenos="inline")
             return highlight(code, lexer, formatter)
 
 
