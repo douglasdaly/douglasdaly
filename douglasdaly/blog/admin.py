2e60039584954d68a2011ac4af842dc046a3604a
Doug <douglas.daly89@gmail.com>
2018-01-05 22:16:29 -0500

    Simplify tags M2M and change Category title to name

--- douglasdaly/blog/admin.py
+++ douglasdaly/blog/admin.py
@@ -4,26 +4,20 @@
 #
 from django.contrib import admin
 
-from .models import Post, Category, Tag, PostToTag
+from .models import Post, Category, Tag
 
 
 #
 #   Admin Classes
 #
 
-class PostToTagInline(admin.TabularInline):
-    model = PostToTag
-    extra = 1
-
-
 class PostAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
-    inlines = [PostToTagInline]
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 class TagAdmin(admin.ModelAdmin):
--- douglasdaly/blog/models.py
+++ douglasdaly/blog/models.py
@@ -18,41 +18,18 @@ from django.urls import reverse
 #   Model Definitions
 #
 
-class Post(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(default="", null=True)
-    body = models.TextField()
-    posted = models.DateTimeField(db_index=True, auto_now_add=True)
-    category = models.ForeignKey('blog.Category', on_delete=models.CASCADE)
-
-    tags = models.ManyToManyField('blog.Tag', through='PostToTag', blank=True)
-
-    class Meta:
-        ordering = ['-posted']
-
-    def __str__(self):
-        return self.title
-
-    def __unicode__(self):
-        return '%s' % self.title
-
-    def get_absolute_url(self):
-        return reverse('view_blog_post', kwargs={'slug': self.slug})
-
-
 class Category(models.Model):
-    title = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
 
     class Meta:
-        ordering = ['title']
+        ordering = ('name',)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def __unicode__(self):
-        return '%s' % self.title
+        return '%s' % self.name
 
     def get_absolute_url(self):
         return reverse('view_blog_category', kwargs={'slug': self.slug})
@@ -63,7 +40,7 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=50, db_index=True)
 
     class Meta:
-        ordering = ['name']
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
@@ -75,12 +52,24 @@ class Tag(models.Model):
         return reverse('view_blog_tag', kwargs={'slug': self.slug})
 
 
-class PostToTag(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, db_index=True)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, db_index=True)
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(default="", null=True)
+    body = models.TextField()
+    posted = models.DateTimeField(db_index=True, auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ('-posted',)
 
     def __str__(self):
-        return self.post.slug + " -> " + self.tag.slug
+        return self.title
 
     def __unicode__(self):
-        return '%s -> %s' % (self.post.slug, self.tag.slug)
+        return '%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('view_blog_post', kwargs={'slug': self.slug})
