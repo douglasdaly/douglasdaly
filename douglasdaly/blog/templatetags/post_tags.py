28955b8274ceab24be3053fa67c3fb192724a99c
Doug <douglas.daly89@gmail.com>
2018-01-09 19:57:02 -0500

    Added page numbers to pagination

--- douglasdaly/blog/templatetags/post_tags.py
+++ douglasdaly/blog/templatetags/post_tags.py
@@ -39,4 +39,16 @@ def post_display(post):
 def post_pagination(post_paginator):
     """ Tag to display page links for posts
     """
-    return {'paginator': post_paginator}
+    start_page = max(post_paginator.number-2, 1)
+    end_page = min(post_paginator.paginator.num_pages,
+                   post_paginator.number + 2) + 1
+
+    if post_paginator.number < 3:
+        end_page = min(post_paginator.paginator.num_pages, 5) + 1
+    if post_paginator.number > post_paginator.paginator.num_pages - 3:
+        start_page = max(post_paginator.paginator.num_pages - 4, 1)
+
+    page_nos = range(start_page, end_page)
+
+    return {'paginator': post_paginator,
+            'page_numbers': page_nos}
