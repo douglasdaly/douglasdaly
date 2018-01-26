2f9d23b2dad04f6f780c4c6a9c656cdd9bd0e222
Doug <douglas.daly89@gmail.com>
2018-01-26 17:03:48 -0500

    Get session variable for selected side menu in blog

--- douglasdaly/blog/views.py
+++ douglasdaly/blog/views.py
@@ -17,25 +17,36 @@ def index(request):
     post_list = Post.objects.all()
     page = request.GET.get("page")
 
-    return render_to_response('blog/index.html', {
+    ret_dict = {
         'blog_settings': blog_settings,
         'posts': __get_post_page(post_list, page=page,
                                  blog_settings=blog_settings),
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/index.html', ret_dict)
 
 
 def view_post(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render_to_response('blog/view_post.html', {
+
+    ret_dict = {
         'post': post,
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/view_post.html', ret_dict)
 
 
 def view_categories(request):
     categories = Category.objects.all()
-    return render_to_response('blog/categories.html', {
+
+    ret_dict = {
         'categories': categories,
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/categories.html', ret_dict)
 
 
 def view_category(request, slug):
@@ -44,18 +55,25 @@ def view_category(request, slug):
     blog_settings = BlogSettings.load()
     page = request.GET.get("page")
 
-    return render_to_response('blog/view_category.html', {
+    ret_dict = {
         'category': category,
         'posts': __get_post_page(Post.objects.filter(category=category), page,
                                  blog_settings),
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/view_category.html', ret_dict)
 
 
 def view_tags(request):
     tags = Tag.objects.all()
-    return render_to_response('blog/tags.html', {
+
+    ret_dict = {
         'tags': tags,
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/tags.html', ret_dict)
 
 
 def view_tag(request, slug):
@@ -64,11 +82,14 @@ def view_tag(request, slug):
     blog_settings = BlogSettings.load()
     page = request.GET.get("page")
 
-    return render_to_response('blog/view_tag.html', {
+    ret_dict = {
         'tag': tag,
         'posts': __get_post_page(Post.objects.filter(tags=tag), page,
                                  blog_settings),
-    })
+    }
+    ret_dict = __append_common_vars(request, ret_dict)
+
+    return render_to_response('blog/view_tag.html', ret_dict)
 
 
 #
@@ -87,3 +108,11 @@ def __get_post_page(post_list, page=1, blog_settings=None):
     post_paginator = Paginator(post_list, per_page)
 
     return post_paginator.get_page(page)
+
+
+def __append_common_vars(request, curr_dict):
+    sort_tab = request.session.get('sort_tab', 'date')
+
+    common_dict = {'sort_tab': sort_tab}
+
+    return {**curr_dict, **common_dict}
