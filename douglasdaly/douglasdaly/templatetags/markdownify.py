dc27504b6b0ba67ddbd305ded9c3984b2aaa8098
Doug <douglas.daly89@gmail.com>
2018-03-19 20:53:18 -0400

    Add ImageAsset referencing to markdownify code

--- douglasdaly/douglasdaly/templatetags/markdownify.py
+++ douglasdaly/douglasdaly/templatetags/markdownify.py
@@ -13,6 +13,9 @@ import mistune
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
+from sorl.thumbnail import get_thumbnail
+
+from douglasdaly.models import ImageAsset
 
 
 #
@@ -23,11 +26,45 @@ class HighlightRenderer(mistune.Renderer):
 
     def block_code(self, code, lang=None):
         if not lang:
-            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+            return '\n<div class="highlight"><pre><code>' + \
+                   '%s</code></pre></div>\n' % mistune.escape(code)
         else:
             lexer = get_lexer_by_name(lang, stripall=True)
-            formatter = HtmlFormatter()
+            formatter = HtmlFormatter(linenos="inline")
             return highlight(code, lexer, formatter)
+
+    def image(self, src, title, text):
+        if src.startswith('asset:'):
+            args = src.split(':')
+            asset_slug = args[1]
+            asset = ImageAsset.objects.filter(slug=asset_slug).first()
+
+            if asset is None:
+                return super().image(src, title, text)
+
+            size = None
+            crop = None
+            quality = 99
+            if len(args) > 2:
+                size = args[2]
+            if len(args) > 3:
+                crop = args[3]
+            if len(args) > 4:
+                quality = int(args[4])
+
+            if size is not None:
+                image_asset = get_thumbnail(asset.asset, size, crop=crop, quality=quality)
+            else:
+                image_asset = asset.asset
+
+            src = image_asset.url
+            if title is None or len(title) == 0:
+                title = asset.title
+
+            if text is None or len(text) == 0:
+                text = asset.description
+
+        return super().image(src, title, text)
 
 
 #
